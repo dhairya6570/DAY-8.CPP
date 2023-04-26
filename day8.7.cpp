68a79222ebea9efda58d873ed2f3fbc20b4a2c01
dhairya6570 <126230153+dhairya6570@users.noreply.github.com>
2023-04-26 12:17:18 +0530

    Add files via upload

---
+++ day8.7.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+class Dhairya{
+	
+	public:
+		
+		~Dhairya(){
+			
+			cout<<"student of R&W"<<endl;
+			
+		}
+		
+		Dhairya(){
+			
+			cout<<"name is Dhairya"<<endl;
+			
+		}
+		
+		
+	
+};
+
+int main(){
+	
+	Dhairya obj;
+	
+	return 0 ;
+}
